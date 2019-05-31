f11b796b62413585c897f25aa0cb1da5f90fe092
imkeVr <ivanrompa@gmail.com>
2019-05-31 16:09:35 +0200

    form

--- wp-content/plugins/employment-widget/employment-widget.php
+++ wp-content/plugins/employment-widget/employment-widget.php
@@ -21,6 +21,52 @@ class Employment_Widget extends WP_Widget {
             )
         );
     }
+    public function widget( $args, $instance ) {
+        extract( $args );
+        extract( $instance );
+
+        echo $before_widget;
+        /* Widget Content Below */
+            echo "You're current state of employment is: [$job]"; 
+        /* Widget Content Above */
+        echo $after_widget;
+    }
+
+    public function form( $instance ) { ?>
+        <?php extract( $instance ); ?>
+        <p>Set your current employment status: </p>
+        <p>
+            <label>
+                <input type="radio" value="unemployed" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'unemployed' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'Unemployed', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="employed" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'employed' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'Employed', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="employed_newChallenge" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'employed_newChallenge' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'Employed but looking for a new challenge', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="freelancer" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'freelancer' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'Freelancer', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="student" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'student' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'Student', 'text_domain' ); ?>
+            </label>
+        </p>
+    <?php }
+
 
 }
 
